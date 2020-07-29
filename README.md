e5675371fa453e58228864b3e8f3008ea634dd30
Mohit Rathore <mrmohitrathoremr@gmail.com>
2020-07-29 09:26:19 +0530

    agenda cmd

---
+++ README.md
@@ -0,0 +1,5 @@
+# Twitch bot for hackbacc
+<a href="https://www.twitch.tv/hackbacc" rel="Twitch Status">![Twitch](https://img.shields.io/twitch/status/hackbacc?color=bluevoilet&style=for-the-badge)</a>
+
+## Commands
+* !agenda - Lists today's agenda.
---
+++ bot.py
@@ -0,0 +1,36 @@
+import os
+from twitchio.ext import commands
+import subprocess
+import datetime
+
+bot = commands.Bot(
+        irc_token="oauth:"+os.environ['tip'],
+    client_id=os.environ['twitch_cid'],
+    nick="hackbacc",
+    prefix='!',
+    initial_channels=['hackbacc']
+)
+
+@bot.event
+async def event_ready():
+    print(f'Ready | {bot.nick}')
+
+@bot.event
+async def event_message(message):
+    print(message.content)
+
+    await bot.handle_commands(message)
+
+@bot.command(name='test', aliases=['t'])
+async def test_command(ctx):
+    await ctx.send(f'Hello {ctx.author.name}')
+
+@bot.command(name='agenda', aliases=['c'])
+async def agenda_command(ctx):
+    date = str(datetime.datetime.now().date())
+    date = "2020-07-26"
+    agenda = subprocess.check_output("curl -s https://raw.githubusercontent.com/hackbacc/schedule/master/json/schedule.json | jq .'projects[] | select(.starts==\"" + date + "\") | .description'", shell=1)#[2:-1]
+    if agenda == b"":
+        agenda = "There is no agenda yet."
+    await ctx.send(agenda.decode('utf-8'))
+bot.run()
